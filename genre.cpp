294e87e8877556294c2f92950ab19410e83a777f
sliesarchukDiana <slesarcukdiana8@gmail.com>
2025-09-02 17:07:29 +0300

    Add main class genre

---
+++ genre.cpp
@@ -0,0 +1,5 @@
+//
+// Created by user on 02.09.2025.
+//
+
+#include "genre.h"
---
+++ genre.h
@@ -0,0 +1,29 @@
+#ifndef DRAFT_GENRE_H
+#define DRAFT_GENRE_H
+#include <string>
+
+class genre {
+private:
+    std::string name;
+    std::string description;
+public:
+    genre();
+    genre(std::string name, std::string description);
+    genre (const genre& other);
+    genre& operator=(const genre& other);
+    genre(genre&& other) noexcept;
+    ~genre();
+
+    std::string getName;
+    std::string getDescription();
+
+    void setName(std::string name);
+    void setDescription(std::string description);
+
+
+
+
+};
+
+
+#endif //DRAFT_GENRE_H
