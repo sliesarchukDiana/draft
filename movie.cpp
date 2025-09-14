74c27a7689cef47230664aac5d48a56efe2f1a4a
sliesarchukDiana <slesarcukdiana8@gmail.com>
2025-09-14 18:37:00 +0300

    Add new atributes to the movie class

--- movie.cpp
+++ movie.cpp
@@ -6,18 +6,18 @@
 int movie::count = 0;
 
 movie::movie()
-    : title("Unknown movie"), director("Unknown"), plot("Unknown"), runningTime(0), budget(0) {
+    : title("Unknown movie"), director("Unknown director"), country("Unknown country"), year (0), budget(0), gross(0), runningTime(0), plot("No info")  {
     std::cout << "Default constructor was called for " << title<<" of parent class movie"<< std::endl;
     count++;
 }
-movie::movie(std::string t, std::string d, std::string p, int rt, int b)
-    : title(std::move(t)), director(std::move(d)), plot(std::move(p)), runningTime(rt), budget(b) {
+movie::movie(std::string t, std::string d, std::string c, int y, int b, int g, int rt, std:: string p)
+    : title(std::move(t)), director(std::move(d)), country(std::move(c)), year(y), budget(b), gross(g), runningTime(rt), plot(std::move(p)) {
     std::cout << "Parameterized constructor was called for " << title <<" of parent class movie"<< std::endl;
     count++;
 }
 
 movie::movie(const movie& other)
-    : title(other.title), director(other.director), plot(other.plot), runningTime(other.runningTime), budget(other.budget) {
+    : title(other.title), director(other.director), country(other.country), year(other.year), budget(other.budget), gross(other.gross), runningTime(other.runningTime), plot(other.plot) {
     count++;
     std::cout << "Copy constructor was called for " << title <<" of parent class movie"<< std::endl;
 }
@@ -25,26 +25,27 @@ movie& movie::operator=(const movie& other) { //copy assignment operator//
     if (this != &other) {
         title = other.title;
         director = other.director;
-        plot = other.plot;
-        runningTime = other.runningTime;
+        country = other.country;
+        year = other.year;
         budget = other.budget;
+        gross = other.gross;
+        runningTime = other.runningTime;
+        plot = other.plot;
     }
     return *this;
 }
 movie::movie(movie&& other) noexcept
     : title(std::move(other.title)),
       director(std::move(other.director)),
-      plot(std::move(other.plot)),
+      country(std::move(other.country)),
+      year(other.year),
+      budget(other.budget),
+      gross(other.gross),
       runningTime(other.runningTime),
-      budget(other.budget) {
+      plot(std::move(other.plot))
+ {
     count++;
     std::cout << "Move constructor was called for " << title <<" of parent class movie"<< std::endl;
-
-    other.title = "Discarded";
-    other.director = "Unknown";
-    other.plot = "Unknown";
-    other.runningTime = 0;
-    other.budget = 0;
 }
 
 movie::~movie() {
@@ -58,15 +59,25 @@ std::string movie::getTitle() const {
 std::string movie::getDirector() const {
     return director;
 }
-std::string movie::getPlot() const {
-    return plot;
+std::string movie::getCountry() const {
+    return country;
 }
-int movie::getRunningTime() const {
-    return runningTime;
+int movie::getYear() const {
+    return year;
 }
 int movie::getBudget() const {
     return budget;
 }
+int movie::getGross() const {
+    return gross;
+}
+int movie::getRunningTime() const {
+    return runningTime;
+}
+std::string movie::getPlot() const {
+    return plot;
+}
+
 
 void movie::setTitle(const std::string& newTitle) {
     title = newTitle;
@@ -74,19 +85,35 @@ void movie::setTitle(const std::string& newTitle) {
 void movie::setDirector(const std::string& newDirector) {
     director = newDirector;
 }
-void movie::setPlot(const std::string& newPlot) {
-    plot = newPlot;
+void movie::setCountry(const std::string& newCountry) {
+    country = newCountry;
+}
+void movie::setYear(int newYear) {
+    if (newYear > 0) //Я хз якє тут обмеження ліпити//
+    {
+        year = newYear;
+    }
+}
+void movie::setBudget(int newBudget) {
+    if (newBudget > 0) {
+        budget = newBudget;
+    }
+}
+void movie::setGross(int newGross) {
+    if (newGross > 0) {
+        gross = newGross;
+    }
 }
 void movie::setRunningTime(int newRunningTime) {
     if (newRunningTime > 0) {
         runningTime = newRunningTime;
     }
 }
-void movie::setBudget(int newBudget) {
-    if (newBudget >= 0) {
-        budget = newBudget;
-    }
+void movie::setPlot(const std::string& newPlot) {
+    plot = newPlot;
 }
+
+
 
 
 int movie::getObjectCount() {
@@ -95,10 +122,14 @@ int movie::getObjectCount() {
 
 std::ostream& operator<<(std::ostream& os, const movie& t) {
     os << "Title: " << t.title << "\n"
-       << "Director: " << t.director << "\n"
-       << "Plot: " << t.plot << "\n"
-       << "Running Time: " << t.runningTime << " minutes\n"
-       << "Budget: $" << t.budget << std::endl;
+    << "Director: " << t.director << "\n"
+    << "Country: " << t.country << "\n"
+    << "Year: " << t.year << "\n"
+    << "Budget: " << t.budget << "\n"
+    <<" Gross: " << t.gross <<"\n"
+    << "Running Time: " << t.runningTime << " minutes\n"
+    << "Plot: " << t.plot << std::endl;
+
     return os;
 }
 
@@ -107,15 +138,17 @@ std::istream& operator>>(std::istream& is, movie& t) {
     std::getline(is, t.title);
     std::cout << "Enter director: ";
     std::getline(is, t.director);
-    std::cout << "Enter plot: ";
-    std::getline(is, t.plot);
-    std::cout << "Enter running time: ";
-    is >> t.runningTime;
+    std::cout << "Enter country: ";
+    std::getline(is, t.country);
+    std::cout << "Enter year: ";
+    is >> t.year;
     std::cout << "Enter budget: ";
     is >> t.budget;
+    std::cout << "Enter gross: ";
+    is >> t.gross;
+    std::cout << "Enter running time: ";
+    is >> t.runningTime;
+    std::cout << "Enter plot: ";
+    std::getline(is, t.plot);
     return is;
-}
-
-int operator+(const movie& lhs, const movie& rhs) {
-    return lhs.getRunningTime() + rhs.getRunningTime();
 }
--- movie.h
+++ movie.h
@@ -4,46 +4,48 @@
 #include <iostream>
 class movie {
 private:
+    static int count;
     std::string title;
     std::string director;
-    std::string plot;
-    int runningTime;
+    std::string country;
+    int year;
     int budget;
-    static int count;
+    int gross;
+    int runningTime;
+    std::string plot;
 public:
     movie();
-    movie(std::string title, std::string director, std::string plot, int runningTime, int budget);
+    movie(std::string title, std::string director, std::string country, int year, int budget, int gross, int runningTime, std::string plot);
     movie(const movie& other); //copy//
     movie(movie&& other) noexcept; //move//
     virtual ~movie();
 
     std::string getTitle() const;
     std::string getDirector() const;
-    std::string getPlot() const;
-    int getRunningTime() const;
+    std::string getCountry() const;
+    int getYear() const;
     int getBudget() const;
+    int getGross() const;
+    int getRunningTime() const;
+    std::string getPlot() const;
 
     static int getObjectCount();
 
     void setTitle(const std::string& newTitle);
     void setDirector(const std::string& newDirector);
-    void setPlot(const std::string& newPlot);
-    void setRunningTime(int newRunningTime);
+    void setCountry(const std::string& newCountry);
+    void setYear(int newYear);
     void setBudget(int newBudget);
+    void setGross(int newGross);
+    void setRunningTime(int newRunningTime);
+    void setPlot(const std::string& newPlot);
 
     friend std::ostream& operator<<(std::ostream& os, const movie& t);
     friend std::istream& operator>>(std::istream& is, movie& t);
-    friend int operator+(const movie& lhs, const movie& rhs);
 
     virtual void displayDetails() const = 0;
 
-    movie& operator=(const movie& other); //copy assignment operator//
-
-    //static binding//
-    void printBasicInfo() const {
-        std::cout << "[Static Binding] Title: " << title
-                  << " | Director: " << director << "\n";
-    }
+    movie& operator=(const movie& other);
 };
 
 #endif // MOVIE_H
